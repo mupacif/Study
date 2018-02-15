cfc83cde0f882141e4b66e27f443290e4d4c7abe
mupacif <mupacif@gmail.com>
2018-02-15 11:51:40 +0100

    ADD JAVASCRIPTI: store 2

--- Tests/JAVASCRIPT/EXERCICES/store_price_compute.js
+++ Tests/JAVASCRIPT/EXERCICES/store_price_compute.js
@@ -1,45 +1,72 @@
+
+//liste des élements de bases
+//qqt : quantités
+//elt : est l'emement html qui sera crée dans le tableau
 var elements = [
     {nom:"chaise",prix:25,qqt:0,elt:null},
     {nom:"Table",prix:150,qqt:0,elt:null},
     {nom:"Meuble",prix:250,qqt:0,elt:null}
 ]
 
-
+// Tableau qui ne peut contenir qu'un seul element
 var panier = new Set();
-
+//html : l'element input qui doit afficher le resultat
 var panierTotal = $("total");
 
-
+/***
+ * retourne le document
+ * @param elt l'element dont il faut retourner le correspondant html
+ * @returns {HTMLElement | null}
+ */
 function $(elt)
 {
     return document.getElementById(elt)
 }
 
+
+//html : tableau pour afficher le panier
 var panierTable = $("panier");
-//ajouter
-//click
+
+
+/***
+ * ajouter ajouter l'élement au tableau
+ * @param num la valeur de l'élement
+ */
 function add(num) {
     //console.log("added"+num)
     addLine(num)
 }
-//addNode
+
+/***
+ * ajoute l'element au tableau
+ * @param num le numbéro de l'élement
+ */
 function addLine(num) {
+    //récupération de l'élement
     var element = elements[num];
 
+    //si le panier ne contient pas déja l'element
     if(! panier.has(element)) {
+       // on crée les élements html
         var row = panierTable.insertRow(); //new tr
         var tdType = row.insertCell(); //first cell
         var tdPrix = row.insertCell(); //first cell
         var tdQtte = row.insertCell(); //first cell
 
+        //on récupère l'élement de la liste
         var element = elements[num];
+        //on incrémente la quantié
         element.qqt = 1;
 
+        // on met les valeurs correspondants
         tdType.innerHTML = element.nom;
         tdPrix.innerHTML = element.prix;
         tdQtte.innerHTML = element.qqt;
 
+        //on ajoute l'élement au tableau
         panier.add(element);
+
+        //on enregistre l'élement
         element.elt = row;
     }else
     {
