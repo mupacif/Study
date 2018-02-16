4fbd6c0f24ceb7ea2012d73b74184c53bf37883d
mupacif <mupacif@gmail.com>
2018-02-16 12:10:19 +0100

    ADD AJAX:zippopotam.us api

---
+++ Tests/JAVASCRIPT/EXERCICES/ajax.js
@@ -0,0 +1,38 @@
+var tmpsUrl = 'temperature.json';
+
+/**
+ * Donwload asyncronosly from given url
+ * @param url
+ * @returns {Promise}
+ */
+function ajx(url) {
+    return new Promise(function(resolve, reject){
+
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+    xhr.send(null);
+
+    xhr.onload = () =>resolve(xhr.responseText)
+    xhr.onerror = () => reject(xhr.statusText);
+
+
+
+})
+
+}
+
+/**
+ * give the json
+ * @param url
+ * @param callback
+ */
+function getJson(url,callback) {
+    ajx(url)
+    .then(value=>callback(eval(`(${value})`)))
+    .catch((error)=>callback(null))
+
+}
+
+
+
